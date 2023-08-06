d49999e77f3a21db713746ef7865e859d211ae85
Owl Burger <28460277+Zebraslive@users.noreply.github.com>
2023-08-06 21:24:05 +0000

    fixes

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -43,32 +43,32 @@ export default function Movie() {
         placeholder='Search for movies'
         searchPath={pathToSearchMovie}
       />
-       {movie ? (
-        <section className="text-gray-400 bg-gray-900 body-font">
-          <div className=" border-gray-800 border border-b-0">
-            <div className="flex flex-wrap -m-2">
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center p-4 rounded-lg">
-                  <img
-                    alt="team"
-                    className="bg-gray-100 flex-shrink-0 mr-4"
-                    src={`${TMDB_IMAGE_ENDPOINT}/${movie.detail.poster_path}`}
-                    style={{ maxWidth: '100px' }}
-                  />
-                  <div className="mt-6 mb-2 text-center md:mt-0 md:mb-4 md:text-left">
-                    <h1 className="mb-1 text-3xl font-light md:mb-3 md:text-5xl">{movie.detail.title}</h1>
-                    <h2 className="text-xs font-light text-app-placeholder sm:text-sm md:text-lg">
-                    {movie.detail.tagline}
-                    </h2>
-                  </div>
-                </div>
-              </div>
+      {movie ? (
+  <section className="text-gray-400 bg-gray-900 body-font">
+    <div className="border-gray-800 border border-b-0 container mx-auto px-4 lg:px-8">
+      <div className="flex flex-wrap -mx-2">
+        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div className="h-full flex items-center p-4 rounded-lg">
+            <img
+              alt="team"
+              className="bg-gray-100 flex-shrink-0 mr-4"
+              src={`${TMDB_IMAGE_ENDPOINT}/${movie.detail.poster_path}`}
+              style={{ maxWidth: '100px' }}
+            />
+            <div className="mt-6 mb-2 text-center md:mt-0 md:mb-4 md:text-left">
+              <h1 className="mb-1 text-3xl font-light md:mb-3 md:text-5xl">{movie.detail.title}</h1>
+              <h2 className="text-xs font-light text-app-placeholder sm:text-sm md:text-lg">
+                {movie.detail.tagline}
+              </h2>
             </div>
           </div>
-        </section>
-      ) : (
-        <Loading />
-      )}
+        </div>
+      </div>
+    </div>
+  </section>
+) : (
+  <Loading />
+)}
       {movie ? (
         
         <section className='flex flex-col sm:mx-8 md:mx-0 md:flex-row md:items-start lg:justify-center'>
